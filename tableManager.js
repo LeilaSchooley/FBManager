76f2649326569a51346faceb5a011ab1ee48030e
Isaac Boadi <46610308+LeilaSchooley@users.noreply.github.com>
2023-07-12 00:56:12 +0100

    Update tableManager.js

--- tableManager.js
+++ tableManager.js
@@ -1,23 +1,23 @@
-// Example usage for creating table headers for groups, accounts, and tasks:
+ 
 
- function createTableHeader(columnNames) {
-  let tableHeader = document.querySelector("thead");
-  if (!tableHeader) tableHeader = document.createElement("thead");
-
-  const tableHeaderRow = document.createElement("tr");
-  
-  columnNames.forEach((columnName) => {
-    const tableHeaderCell = document.createElement("th");
-    tableHeaderCell.textContent = columnName;
-    tableHeaderRow.appendChild(tableHeaderCell);
-  });
+// Function to create table header
+function createTableHeader(columnNames) {
   let table = document.querySelector("table");
+  let tableHeader = document.querySelector("thead");
+  if (!tableHeader) {
+    tableHeader = document.createElement("thead");
+    table.appendChild(tableHeader);
+  }
+  
+  tableHeader.innerHTML = "";
 
-  tableHeader.appendChild(tableHeaderRow);
-
-  table.appendChild(tableHeader)
-
-  return table;
+  const headerRow = document.createElement("tr");
+  columnNames.forEach((columnName) => {
+    const headerCell = document.createElement("th");
+    headerCell.textContent = columnName;
+    headerRow.appendChild(headerCell);
+  });
+  tableHeader.appendChild(headerRow);
 }
 
 
